e2db2594f2c950c6e4f89100164c13cd5750c4c7
sunidhi17 <sunidhisachan17@gmail.com>
2023-07-13 23:17:11 +0530

    photo editor app added

---
+++ src/Sidebaritem.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Sidebaritem = ({ name, active, handleClick }) => {
+  return (
+      <button 
+      className={`sidebar-item ${active ? 'active' : ''}`}
+      onClick={handleClick}
+    >
+      {name}
+    </button>
+  )
+}
+
+export default Sidebaritem;
